bbf1943cf868c4b67cc130c5db7ad81aaa5a11a8
Gloomy Ghost <740291272@qq.com>
2020-03-22 15:08:07 +0800

    Create langU.h

---
+++ include/langU.h
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#define 开始 int main(){
+#define 复读 std::cout <<
+#define 继续复读 <<
+#define 复读完 << std::endl;
+#define 加 +
+#define 减 -
+#define 乘 *
+#define 除 /
+#define 结束 ;return 0;}
